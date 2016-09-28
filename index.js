dcee51ef364edd2186eed7f8e9d0bf448b9312e0
Hien Tran <aznketchup@yahoo.com>
2016-09-27 18:02:19 -0700

    Add functions to check connections.
    -Track status of board inside index.js.
    -Implement function to check connection vertically.
    -Implement function to check connection horizontally.
    -Implement function to check minor diagonal connection.
    -Implement function to check major diagonal connection.
    -Check connection when a piece is placed.
    -Initialize mocha testing.

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-var color = 'red'
+var color = 'red';
+var board = [];
+var rows = 6;
+var columns = 6;
+var connectionsNeeded = 4;
+
+for (var i = 0; i < rows; i++) {
+  board.push([]);
+  for (var j = 0; j < columns; j++) {
+    board[i].push(false);
+  }
+} 
 
 document.addEventListener('DOMContentLoaded', function(){
   document.getElementById('container').onclick = function(event){
     var column = event.target.classList.contains('first');
-    if (column){
-      var children = event.target.parentElement.children;
+    var parent = event.target.parentElement;
+    var columnNumber = Number(parent.getAttribute('id')) - 1;
+    if (column) {
+      var children = parent.children;
       var condition = true; 
-      for(var i = children.length - 1; i >= 0 && condition ; i--) {
-        if(children[i].className !== 'red' && children[i].className !== 'yellow') {
-          children[i].className = color;
+      for (var rowNumber = children.length - 1; rowNumber >= 0 && condition ; rowNumber--) {
+        if (board[rowNumber][columnNumber] !== 'red' && board[rowNumber][columnNumber] !== 'yellow') {
+          children[rowNumber].className = color;
+          board[rowNumber][columnNumber] = color;
+          if (horizontalConnection(rowNumber, color)) {
+            console.log('connection made horizontally');
+          }
+          if (verticalConnection(columnNumber, color)) {
+            console.log('connection made vertically');
+          }
+          if (minorDiagonalConnection(rowNumber, columnNumber, color)) {
+            console.log('connection made from minor diagonal');
+          }
+          if (majorDiagonalConnection(rowNumber, columnNumber, color)) {
+            console.log('connection made from major diagonal');
+          }
           condition = false;
         }
       }
@@ -16,3 +42,83 @@ document.addEventListener('DOMContentLoaded', function(){
     }
   };
 });
+
+var horizontalConnection = function (rowNumber, color) {
+  var maxConnection = 0;
+  var runningConnection = 0;
+  for (var columnNumber = 0; columnNumber < columns; columnNumber++ ) {
+    if (board[rowNumber][columnNumber] === color) {
+      runningConnection++;
+    } else {
+      maxConnection = runningConnection > maxConnection ? runningConnection : maxConnection; 
+      runningConnection = 0;
+    }
+  }
+  return maxConnection >= connectionsNeeded || runningConnection >= connectionsNeeded ? true : false;
+};
+
+var verticalConnection = function (columnNumber, color) {
+  var maxConnection = 0;
+  var runningConnection = 0;
+  for (var rowNumber = 0; rowNumber < rows; rowNumber++ ) {
+    if (board[rowNumber][columnNumber] === color) {
+      runningConnection++;
+    } else {
+      maxConnection = runningConnection > maxConnection ? runningConnection : maxConnection; 
+      runningConnection = 0;
+    }
+  }
+  return maxConnection >= connectionsNeeded || runningConnection >= connectionsNeeded ? true : false;
+};
+
+var minorDiagonalConnection = function (rowNumber, columnNumber, color) {
+  var minorIndex = rowNumber + columnNumber;
+  var maxConnection = 0;
+  var runningConnection = 0;
+  var min = connectionsNeeded - 1;
+  var max = (rows - 1) + (columns - 1) - connectionsNeeded + 1;
+
+  //We can ignore some minor diagonals that doesn't meet the connections require to win.
+  if(minorIndex >= min && minorIndex <= max) {
+    for (var rowNumber = 0; rowNumber < rows; rowNumber++ ) {
+      var row = rowNumber;
+      var column = minorIndex - row;
+      if (column < rows) {
+        if (board[row][column] === color) {
+          runningConnection++;
+        } else {
+          maxConnection = runningConnection > maxConnection ? runningConnection : maxConnection; 
+          runningConnection = 0;
+        }
+      } 
+    }
+    return maxConnection >= connectionsNeeded || runningConnection >= connectionsNeeded ? true : false;
+  }
+  return false;
+};
+
+var majorDiagonalConnection = function (rowNumber, columnNumber, color) {
+  var majorIndex = rowNumber - columnNumber;
+  var maxConnection = 0;
+  var runningConnection = 0;
+  var min = 0 - rows + connectionsNeeded;
+  var max = 0 + rows - connectionsNeeded;
+
+  //We can ignore some major diagonals that doesn't meet the connections require to win.
+  if(majorIndex >= min && majorIndex <= max) {
+    for (var rowNumber = 0; rowNumber < rows; rowNumber++ ) {
+      var row = rowNumber;
+      var column = row - majorIndex;
+      if (column < rows) {
+        if (board[row][column] === color) {
+          runningConnection++;
+        } else {
+          maxConnection = runningConnection > maxConnection ? runningConnection : maxConnection; 
+          runningConnection = 0;
+        }
+      } 
+    }
+    return maxConnection >= connectionsNeeded || runningConnection >= connectionsNeeded ? true : false;
+  }
+  return false;
+}
